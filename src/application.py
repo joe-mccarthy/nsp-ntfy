929ecc3337656c421301ce7d25a8f426b4b140b8
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-09-22 13:37:21 +0100

    initial project set up

---
+++ src/application.py
@@ -0,0 +1,6 @@
+
+def random_method():
+    return True
+
+if __name__ == "__main__":
+    print("nsp-ntfy")
---
+++ tests/test_nothing.py
@@ -0,0 +1,4 @@
+from src.application import random_method
+
+def test_nothing():
+    assert random_method() == True
